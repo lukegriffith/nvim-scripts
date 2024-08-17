b69ac37a7cf8fff722badca31f18e5adccb8b067
Luke Griffith <lukemgriffith@gmail.com>
2024-08-17 19:07:26 +0100

    Changing how bare repo is checkedo ut

--- setup-nvim-container.sh
+++ setup-nvim-container.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /root/bare_config /root/.config/nvim
 git clone --bare https://github.com/lukegriffith/kickstart.nvim /root/bare_config
-/usr/bin/git --git-dir=/root/bare_config --work-tree=/root/.config/nvim fetch --all
-/usr/bin/git --git-dir=/root/bare_config --work-tree=/root/.config/nvim pull
+git --git-dir=/root/bare_config --work-tree=/root/.config/nvim pull
+git --git-dir=/root/bare_config --work-tree=/root/.config/nvim checkout -f
 
